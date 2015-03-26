30dd4ed2fbbb8303b8ecb942f9d59d1d0e850ada
Leah Padgett <lmp0024@yahoo.com>
2015-03-26 16:46:10 -0400

    Completed if, else, ternary

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -14,6 +14,21 @@
     NSString *itemToReturn;
 
     /* WORK HERE */
+    if (dollars >= 100000000){
+        itemToReturn = @"have The Big Apple";
+    }
+    else if (dollars >= 1000) {
+        itemToReturn = @"have an Apple computer";
+    }
+    else if (dollars >= 6){
+        itemToReturn = @"have an apple";
+    }
+    else if (dollars >= 5) {
+        itemToReturn = @"have some gum";
+        
+    } else {
+        itemToReturn = @"get out of my store";
+    }
     
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     return itemToReturn;
@@ -27,8 +42,8 @@
     if (self.getsDiscount) {
         cost *= .75;
     }
-    
-    return cost;
+  
+    return self.getsDiscount ? 18 : 24;;
 }
 
 @end
