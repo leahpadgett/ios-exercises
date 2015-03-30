2662f316717a6652e6bad6bdb24c24a879182994
Leah Padgett <lmp0024@yahoo.com>
2015-03-30 16:36:30 -0400

    Completed Arrays

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -12,22 +12,31 @@
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
     /* WORK HERE */
-    return @[];
+    return [characterString componentsSeparatedByString:@";"];
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @"";
+    return  [characterArray componentsJoinedByString:@";"];
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @[];
+    NSArray *alphabeticalArray = [characterArray sortedArrayUsingSelector:@selector(caseInsensitiveCompare:)];
+    return alphabeticalArray;
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
     /* WORK HERE */
+    for (NSString *characterName in characterArray) {
+        NSString *lowercaseName = characterName.lowercaseString;
+        NSRange worfRange = [lowercaseName rangeOfString:@"worf"];
+        
+        if (worfRange.location != NSNotFound) {
+            return YES;
+        }
+    }
+    
     return NO;
 }
-
 @end
